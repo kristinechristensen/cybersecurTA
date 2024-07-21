0ebac9bb08727e6167ce7dec8522c08b591ffee3
Kristine Christensen <67657119+kristinechristensen@users.noreply.github.com>
2024-07-21 09:54:56 -0500

    updatestoerror

--- app/api/auth/error/page.js
+++ app/api/auth/error/page.js
@@ -4,7 +4,7 @@ export default function AuthErrorPage(){
  
   return (
     <div className="container h-screen flex items-center justify-center items-center flex-col gap-6">
-      <h1 className="text-3xl text-white font-bold"> Ups, it seems that you don't have an account 👾</h1>
+      <h1 className="text-3xl text-blue font-bold"> Oops, it seems that you don't have an account</h1>
     </div>
   )
 }
